dd32ac951b16a0ac57f5c89c98421fa960f3d019
MohammadKN <85948201+MohammadKN@users.noreply.github.com>
2021-06-23 01:37:12 +0300

    Making the app able to scan barcodes.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,14 +14,77 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      home:Scaffold(
+        body: HomePage(),
+      ),
+    );
+  }
+
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  String? scanResult;
+
+  @override
+  Widget build(BuildContext context) {
+    return NeumorphicApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: null,
         body: Center(
-          child: Text(
-              'Barcode Scanner'
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                padding: EdgeInsets.all(20),
+                child: Text(
+                  'Barcode Scanner',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              NeumorphicButton(
+                margin: EdgeInsets.all(20),
+                padding: EdgeInsets.all(15),
+                onPressed: () {scanBarcode();},
+                child: Text('Scan Barcode'),
+              ),
+              Container(
+                child: Text(
+                    scanResult == null
+                        ?'Scan a code!'
+                        :'Scan Results:$scanResult',
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
     );
   }
+  Future scanBarcode() async {
+    String scanResult;
+    try {
+      scanResult = await FlutterBarcodeScanner.scanBarcode(
+          "#ffffff", "Cancel", true, ScanMode.BARCODE);
+    }on PlatformException {
+      scanResult = 'failed';
+    }
+    if (!mounted) return;
+    setState(() => this.scanResult = scanResult);
+  }
 }
+
+
+
